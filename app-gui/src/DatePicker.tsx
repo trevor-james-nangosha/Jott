9eb2c979d891726dce5b9bc0f80df2ad088d385e
trevor james nangosha <trevornangosha16@gmail.com>
2023-04-18 12:21:32 +0300

    Added DatePicker props

--- app-gui/src/DatePicker.tsx
+++ app-gui/src/DatePicker.tsx
@@ -1,12 +1,22 @@
-import dayjs from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import { StaticDatePicker } from '@mui/x-date-pickers/StaticDatePicker';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 
-function DatePicker(){
+import customParseFormat from 'dayjs/plugin/customParseFormat'
+dayjs.extend(customParseFormat)
+
+export interface DatePickerProps{
+    date?: Dayjs;
+    handleDateChange: (value: any) => void
+
+}
+
+function DatePicker(props: DatePickerProps){
+
     return <div className="date-picker">
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <StaticDatePicker defaultValue={dayjs}/>
+            <StaticDatePicker value={props.date} disableFuture onChange={props.handleDateChange} />
         </LocalizationProvider>
     </div>
 }
