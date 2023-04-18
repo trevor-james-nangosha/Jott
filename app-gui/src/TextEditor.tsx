39bf99424511fcfaf57daaa56a39ef0b921a54d1
trevor james nangosha <trevornangosha16@gmail.com>
2023-04-18 12:19:08 +0300

    Added TextEditorProps

--- app-gui/src/TextEditor.tsx
+++ app-gui/src/TextEditor.tsx
@@ -1,9 +1,20 @@
-import { useState } from 'react';
+import { DeltaStatic } from 'quill';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import SaveButton from './SaveButton';
+export interface TextEditorProps{
+    defaultValue?: string;
+    value?: string;
+    handleContentChange?: (value: string, delta: DeltaStatic, source: any, editor: ReactQuill.UnprivilegedEditor) => void
+    className?: string;
+    onClick?: () => void
+  }
 
-export default function TextEditor(){
-    const [value, setValue] = useState(" ");
-    return <ReactQuill className="text-editor" theme="snow" value={value} onChange={setValue} />
+export default function TextEditor(props: TextEditorProps){
+    return (
+        <div className={props.className}>
+            <ReactQuill className="text-editor" theme="snow" defaultValue={props.defaultValue} onChange={props.handleContentChange} />
+            <SaveButton onClick={props.onClick} />
+        </div>)
 }
 
