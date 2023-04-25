728a40ac6d142a68b33a20c321b82a2ce0015b92
trevor james nangosha <trevornangosha16@gmail.com>
2023-04-25 13:48:29 +0300

    Refactored code. Opted for controlled component.
    Was having issues with the use of uncontrolled components.
    Added some state fields to workaround these.

--- app-gui/src/App.tsx
+++ app-gui/src/App.tsx
@@ -2,114 +2,104 @@ import TextEditor from './TextEditor';
 import DatePicker from './DatePicker';
 import { JournalEntry } from '@jott/lib/types'
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { DeltaStatic } from 'quill';
 import ReactQuill from 'react-quill';
 import dayjs, { Dayjs } from 'dayjs';
 import { v4 as uuidv4 } from 'uuid';
 import config from './config';
 
-function App() {
-  const todayEntryId = uuidv4()
-  const [entryState, setEntryState] = useState<JournalEntry>({ id: todayEntryId, date: dayjs()})
-  const [todayEntryState, setTodayEntryState] = useState<JournalEntry>({ id: todayEntryId, date: dayjs(), content: "today-default"})  
+// for some reason which could be with the implementation of the ReactQuill component
+// it is quite difficult using useState inside the editor change handler function
+// workaround is to use some kind of global variable
+let globalText: string;
 
-  const isDateToday = (date: Dayjs): boolean => {
-    return dayjs().diff(date, "day") <= 0;
-  }
+const onContentChange = (value: string, delta: DeltaStatic, source: any, editor: ReactQuill.UnprivilegedEditor) => {
+  globalText = editor.getText()
+}
 
-  const getEntryAtDate = async (date: Dayjs): Promise<JournalEntry | void> => {
+const saveEntryToDb = async (entry: JournalEntry) => {
+  const date = entry.date.toISOString()
+
     try {
-      const response = await fetch(`http://${config.HOST}:${config.PORT}/entries?date=${date.toISOString().slice(0, 10)}`, {
-        headers: { Accept: 'application/json' },
-      });
-      const data =  await response.json() as any[] // this is right 'cause what you get back is an array of objects
-      console.log(data) /////////
-
-      if (data.length) {
-        return makeJournalEntry(data[0])
-      }
-    } catch (error) {
-      return console.error(error);
-    }
-  }
-
-  const makeJournalEntry = (data: any): JournalEntry => {
-    let entry: JournalEntry;
-    
-    try {
-      let obj = JSON.parse(data)
-      console.log(dayjs(obj.date)) /////////////
-      entry = {id: obj.id, content: obj.content, date: dayjs(obj.date)}
-    } catch (error) {
-      entry = {id: data.id, content: data.content, date: dayjs(data.date)}
-    }
-    console.log(entry) /////////////////
-
-    return entry;
-  }
-
-  const handleDateChange = async (e: any) => {
-    const newDate = dayjs(e.$d)
-
-    if (!isDateToday(newDate)) {
-      const entry = await getEntryAtDate(newDate);
-
-      if (entry) {
-        setEntryState({...entry})
-      } else {
-        // we update the state, generate a new id for this date
-        setEntryState({id: uuidv4(), 'date': newDate})
-      }
-
-    } else {
-      setEntryState(todayEntryState)
-    }
-  }
-
-  const handleContentChange = (value: string, delta: DeltaStatic, source: any, editor: ReactQuill.UnprivilegedEditor) => {
-    if (!isDateToday(entryState.date as Dayjs)) {
-      setEntryState((oldState) => {
-        return {...oldState, 'content': editor.getText()}
-      })
-    } else {
-      setTodayEntryState(oldState => {
-        return {...oldState, 'content': editor.getText()}
-      })
-
-      // editor.getContents()
-      
-    }
-  }
-
-  const saveEntry = async (): Promise<void> => {
-    const date = entryState['date'] as Dayjs
-    try {
-      await fetch(`http://${config.HOST}:${config.PORT}/entries?date=${date.toISOString()}`, {
+      await fetch(`http://${config.HOST}:${config.PORT}/entries?date=${date}`, {
         method: 'POST',
         headers: { Accept: 'application/json', 'Content-Type': 'application/json' },
-        body: JSON.stringify(entryState)
+        body: JSON.stringify(entry)
       });
     } catch (error) {
       console.error(error);
     }
+}
+
+function App() {
+  
+  
+  // we do not want the id of the current day to change when you select another date entry
+  const todayEntryId = uuidv4()  
+  const [state, setState] = useState<JournalEntry>({
+    id: todayEntryId,
+    date: dayjs(),
+    content: "", 
+    savedContent: ""
+  })
+
+  const [todayState, setTodayState] = useState<JournalEntry>({
+    id: todayEntryId,
+    date: dayjs(),
+    content: "", 
+    savedContent: ""
+  })
+
+  const [savedContent, setSavedContent] = useState("")
+
+  // also, for some reason, we seem to be making two calls to the db
+  // it could be that the first time when the editor is rendered, we also perform a fetch call
+  // i do want to send an empty content string back to the database.
+  // this 'changeCount' flag takes care of that.
+  const [changeCount, setChangeCount] = useState(0)
+
+  useEffect(() => {
+    setState(prevState => {
+      return {...prevState, savedContent: savedContent, content: savedContent}
+    })
+  }, [savedContent])
+
+  useEffect(() => {
+    if (changeCount > 1) {
+      saveEntryToDb(state)   
+    }
+    setChangeCount(count => count + 1)
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [state.content])
+  
+
+  const handleSaveButtonClicked = async () => {
+    setSavedContent(globalText)
   }
 
-  if (!isDateToday(entryState.date as Dayjs)) {
-    return (
-      <div className="App">
-        <DatePicker date={entryState.date} handleDateChange={handleDateChange} />
-        <TextEditor handleContentChange={handleContentChange} className="text-editor-container" onClick={saveEntry} defaultValue={entryState.content} />
-      </div>
-    );
-  } 
+  const handleDateChange = () => {
+    console.log("hello, date.")
+  }
 
+ 
   return (
     <div className="App">
-      <DatePicker date={todayEntryState.date} handleDateChange={handleDateChange} />
-      <TextEditor handleContentChange={handleContentChange} className="text-editor-container" onClick={saveEntry} defaultValue={todayEntryState.content} />
+      <DatePicker date={dayjs()} handleDateChange={handleDateChange} />
+      <TextEditor handleContentChange={onContentChange} 
+                  className="text-editor-container" 
+                  onClick={handleSaveButtonClicked}
+                  value={state.content}/>
     </div>
   );
 }
 
 export default App;
+
+
+// user clicks save ----> we update the state of the text editor -------->
+// save the entry to the db -------> 
+
+// the date changes -----------> generate a new ID, update the state ---------------->
+// 
