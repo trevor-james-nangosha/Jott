4bb3b101d28c391ac03f7f5b3f37c3790c6edde2
trevor james nangosha <trevornangosha16@gmail.com>
2023-04-27 14:55:52 +0300

    Added DB error codes enum

--- lib/types.ts
+++ lib/types.ts
@@ -10,3 +10,8 @@ export interface JournalEntry extends JournalBase{
     createdAt?:Date;
     updatedAt?: Date;
 }
+
+export enum DB_ERROR {
+    ECONNREFUSED            = "ECONNREFUSED",
+    ER_NO_SUCH_TABLE      = "ER_NO_SUCH_TABLE"
+}
